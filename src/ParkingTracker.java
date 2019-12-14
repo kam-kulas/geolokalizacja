7607b18c771fe27c11cd1347c7cf42d5d9191a77
Mirowski <m.mirowski21@gmail.com>
2019-12-14 17:23:38 +0100

    Rzutowanie na int liczby wolnych miejsc

--- src/ParkingTracker.java
+++ src/ParkingTracker.java
@@ -15,7 +15,36 @@ public class ParkingTracker extends Agent {
         YP = generator.nextDouble()*100;
         CAPACITY = generator.nextInt(200);
         freeSpaces = (int) 0.3 * generator.nextInt(CAPACITY);
-        System.out.println("Agent: "+getLocalName()+ " is ready for work!");
+
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+
+        for (int i=0; i<agents.length;i++)
+            msg.addReceiver( agents[i].getName() );
+
+        msg.setLanguage("Polski");
+        msg.setOntology("Geolokalizacja");
+
+        System.out.println("Agent: "+getLocalName()+ " jest gotowy do pracy!");
+        addBehaviour(new TickerBehaviour(this, 10000){
+            protected void onTick(){
+                if (freeSpaces != 0) {
+                    msg.setContent("" + freeSpaces + " są dostępne na ");
+                }
+                else if (freeSpaces == 0) {
+                    msg.setContent("Miejsca parkingowe nie są dostępne");
+                }
+                send(msg);
+            }
+        })
+
+        addBehaviour(new CyclicBehaviour(this) {
+            public void action() {
+                ACLMessage msg= receive();
+                if (msg!=null)
+                    System.out.println( "== Odpowiedź" + " <- " +  msg.getContent() + " od " +  msg.getSender().getName());
+                block();
+            }
+        });
     }
 
     public boolean isFreeSpaceAvailable() {
@@ -25,12 +54,12 @@ public class ParkingTracker extends Agent {
         return true;
     }
 
-    public static int[] getLocalization() {
-        int[] coordinates = {XP, YP};
+    public static double[] getLocalization() {
+        double[] coordinates = {XP, YP};
         return coordinates;
     }
 
     protected void takeDown(){
-        System.out.println("Agent: "+getLocalName()+ " is done.");
+        System.out.println("Agent: "+getLocalName()+ " skończył pracę.");
     }
 }
