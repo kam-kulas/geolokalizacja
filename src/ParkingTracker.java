847fc099865e5924f4b5e338633afb3b0814f3ab
Mirowski <m.mirowski21@gmail.com>
2019-12-14 16:12:22 +0100

    ParkingTracker basic fields, methods

--- src/ParkingTracker.java
+++ src/ParkingTracker.java
@@ -1,9 +1,34 @@
 import jade.core.Agent;
 
 public class ParkingTracker extends Agent {
+    private static final double XP;
+    private static final double YP;
+    private static final int CAPACITY;
+    private int freeSpaces;
 
     @Override
     protected void setup(){
-        System.out.println("Agent: "+getLocalName());
+        Random generator = new Random();
+        XP = generator.nextDouble()*100;
+        YP = generator.nextDouble()*100;
+        CAPACITY = generator.nexInt(200);
+        freeSpaces = 0.3 * generator.nextInt(CAPACITY);
+        System.out.println("Agent: "+getLocalName()+ " is ready for work!");
+    }
+
+    public boolean isFreeSpaceAvailable() {
+        if (freeSpaces == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int[] getLocalization() {
+        int[] coordinates = {XP, YP};
+        return coordinates;
+    }
+
+    protected void takeDown(){
+        System.out.println("Agent: "+getLocalName()+ " is done.");
     }
 }
